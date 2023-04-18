5d1bc1ce80c49dcecf30bc8e8d521f146f73b091
Camilo Montiel <103394512+CamiloMontiel99@users.noreply.github.com>
2023-04-18 16:47:30 -0500

    Add files via upload

---
+++ VideoJuego1/JS/game.js
@@ -0,0 +1,51 @@
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+let canvasSize;
+let elementSize;
+
+function setCanvasSize(){
+    if(window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8;
+    } else {
+        canvasSize =window.innerHeight *0.8;
+    }
+    
+    canvas.setAttribute('width', canvasSize);
+    canvas.setAttribute('height', canvasSize);
+    
+    elementSize = canvasSize / 10;
+    
+    startGame();
+}
+
+function startGame(){
+    game.font = `${elementSize - 8}px Verdana`;
+    game.textAlign = 'end';
+
+    for (let i = 1; i <= 10; i++){
+        for (let h = 1; h <= 10; h++){
+        game.fillText(emojis['X'], elementSize * i + 5, elementSize * h - 10);  
+        }
+    }
+}
+
+
+
+
+
+    // window.innerHeight
+    // window.innerWidth
+    
+    // game.fillRect(0,0,100,100);
+    // game.clearRect(0,0,50,50);
+    // game.clearRect();
+    // game.clearRect(0, 0, 50, 50);
+
+    // game.font = '25px Verdana';
+    // game.fillStyle = 'purple';
+    // game.textAlign = 'center';
+    // game.fillText('platzi', 50, 50);
